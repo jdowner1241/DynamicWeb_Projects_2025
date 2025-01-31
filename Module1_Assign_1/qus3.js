480d3dfbcf2e96c87cba7419d14e1fd447e5f14b
jdowner1241 <109806567+jdowner1241@users.noreply.github.com>
2025-01-31 13:25:15 -0500

    Module 1 Assignment 1. Completed 1 to 5.

--- Module1_Assign_1/qus3.js
+++ Module1_Assign_1/qus3.js
@@ -1,15 +1,26 @@
+let toggle = false;
+
 function qus3() {
     let p = document.getElementsByTagName("p");
     let textArea = document.getElementsByTagName("textarea");
 
-    for (let i = 0; i < p.length; i++ ) {
-        p[i].style.border = "3px solid red";
-    }
-
-    for (let i = 0; i < textArea.length; i++ ) {
-        textArea[i].style.border = "3px solid red";
-    }
+    if(toggle == false){
+        for (let i = 0; i < p.length; i++ ) {
+            p[i].style.border = "3px solid red";
+        }
     
-
+        for (let i = 0; i < textArea.length; i++ ) {
+            textArea[i].style.border = "3px solid red";
+        }
+        toggle = true;
+    } else {
+        for (let i = 0; i < p.length; i++ ) {
+            p[i].style.border = "none";
+        }
+    
+        for (let i = 0; i < textArea.length; i++ ) {
+            textArea[i].style.border = "none";
+        }
+        toggle = false;
+    }
 }
-
