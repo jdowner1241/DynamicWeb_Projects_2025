1ed2e1634aa0187c1aed2fe2188abb87ce5a579d
jdowner1241 <109806567+jdowner1241@users.noreply.github.com>
2025-02-26 16:23:37 -0500

    Folder updated

--- Module1_Assign_1 - Orginal Design/qus2.js
+++
@@ -1,9 +0,0 @@
-function qus2() {
-    let firstName = "", lastName = "", age = "";
-
-    firstName = prompt("Enter your First Name : ");
-    lastName = prompt("Enter your Last Name : ");
-    age = prompt("Enter your Age Name : ");
-
-    alert(`Hello ${firstName} ${lastName}, I now know your ${age} years old.`);
-}
